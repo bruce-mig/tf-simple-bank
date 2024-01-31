b08f08c1134031992d3968482f72a547fa84ad02
bruce-mig <h180009t@hit.ac.zw>
2024-01-31 11:05:10 +0200

    updated provider for prod env

--- prod/providers.tf
+++ prod/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.33.0"
+      version = "5.34.0"
     }
   }
   #   backend "s3" {
   #     bucket = "terraform-backend-31012024"
-  #     key = "simplebank_backend.tfstate"
+  #     key = "simplebank_prod_backend.tfstate"
   #     region = var.region
   #     encrypt = true
   #     dynamodb_table = "terraform-lock-table"
