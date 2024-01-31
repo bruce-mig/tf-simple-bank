d0e93953053b20bbd37139d22d00277b7e33b65f
bruce-mig <h180009t@hit.ac.zw>
2024-01-31 20:39:25 +0200

    changed cidr block default

--- modules/simplebank/main.tf
+++ modules/simplebank/main.tf
@@ -18,17 +18,14 @@ module "simplebank_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = terraform.workspace
-  cidr = "${lookup(var.network_prefix, terraform.workspace, "10.1")}.0.0/16"
+  cidr = "${lookup(var.network_prefix, terraform.workspace, "10.0")}.0.0/16"
 
   azs = var.azs
   public_subnets = [
-    "${lookup(var.network_prefix, terraform.workspace, "10.1")}.101.0/24",
-    "${lookup(var.network_prefix, terraform.workspace, "10.1")}.102.0/24",
-    "${lookup(var.network_prefix, terraform.workspace, "10.1")}.103.0/24"
+    "${lookup(var.network_prefix, terraform.workspace, "10.0")}.101.0/24",
+    "${lookup(var.network_prefix, terraform.workspace, "10.0")}.102.0/24",
+    "${lookup(var.network_prefix, terraform.workspace, "10.0")}.103.0/24"
   ]
-
-  enable_nat_gateway = true
-
 
   tags = {
     Terraform   = "true"
@@ -36,70 +33,25 @@ module "simplebank_vpc" {
   }
 }
 
-# module "autoscaling" {
-#   source  = "terraform-aws-modules/autoscaling/aws"
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-#   name     =  "${var.environment.name}-simplebank"
-#   min_size = var.asg_min_size
-#   max_size = var.asg_max_size
+  name = terraform.workspace
 
-#   vpc_zone_identifier = module.simplebank_vpc.public_subnets
-#   target_group_arns   = module.simplebank_alb.target_groups.arn
-#   security_groups     = [ module.simplebank_sg.security_group_id ]
-
-#   key_name = aws_key_pair.ssh-key.key_name
-
-#   image_id      = data.aws_ami.app_ami.id
-#   instance_type = var.instance_type
-
-#   user_data = file("entry-script.sh")
-
-#   tags = {
-#     Name = "${var.environment.name}-server"
-#   }
-# }
-
-resource "aws_instance" "simplebank-app" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  subnet_id              = module.simplebank_vpc.public_subnets[0]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.test_ssh-key.key_name
   vpc_security_group_ids = [module.simplebank_sg.security_group_id]
-  availability_zone      = module.simplebank_vpc.azs[0]
-
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.test_ssh-key.key_name
+  subnet_id              = module.simplebank_vpc.public_subnets[0]
 
   user_data = file("../entry-script.sh")
-
-  # connection {
-  #   type = "ssh"
-  #   host = self.public_ip
-  #   user = var.ec2_user
-  #   private_key = file(var.private_key_location)
-  # }
-
-  # provisioner "file" {
-  #   source = "entry-script.sh"
-  #   destination = "/home/${var.ec2_user}/entry-script.sh"
-  # }
-
-  # provisioner "remote-exec" {
-  #   script = file("entry-script.sh")
-  #   on_failure = fail
-  # }
-
-  # provisioner "local-exec" {
-  #   command = "echo ${self.public_ip} > output.txt"
-  # }
-
   tags = {
-    Name = "${terraform.workspace}-server"
+    Terraform   = "true"
+    Environment = terraform.workspace
   }
 
   depends_on = [module.simplebank_postgres_db]
 }
-
 
 module "simplebank_alb" {
   source = "terraform-aws-modules/alb/aws"
@@ -111,22 +63,6 @@ module "simplebank_alb" {
   # Security Group
   security_groups = [module.simplebank_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
   target_groups = {
     ex-instance = {
       name_prefix = "h1"
@@ -137,18 +73,17 @@ module "simplebank_alb" {
   }
 
   tags = {
+    Terraform   = "true"
     Environment = terraform.workspace
-    Project     = "Example"
   }
 }
 
 module "simplebank_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  source = "terraform-aws-modules/security-group/aws"
 
   name                = "${terraform.workspace}-simplebank"
   vpc_id              = module.simplebank_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = var.sg_ingress_rules
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
@@ -166,14 +101,14 @@ module "simplebank_postgres_db" {
 
   engine               = "postgres"
   engine_version       = "16"
-  family               = "postgres16" # DB parameter group
-  major_engine_version = "16"         # DB option group
+  family               = "postgres16"
+  major_engine_version = "16"
   instance_class       = "db.t4g.micro"
 
   allocated_storage     = 1
   max_allocated_storage = 5
 
-  db_name  = "simple-bank"
+  db_name  = var.db_name
   username = "root"
   password = var.db_password
   port     = 5432
